eb104dcfa50d8906757333d25bdeb8dd00ef36bf
adhfmz7 <ahmadaldasouqi559@gmail.com>
2025-08-12 02:25:35 -0700

    update misc

---
+++ misc/README.md
@@ -0,0 +1,6 @@
+# Misc
+
+Unrelated code for my own learning.
+
+## Contents:
+- PPO Implementation - A toy implementation of ppo
--- misc/ppo.py
+++ misc/ppo.py
@@ -1,6 +1,25 @@
 
 
-def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0, 
-        steps_per_epoch=4000, epochs=50, gamma=0.99, clip_ratio=0.2, pi_lr=3e-4,
-        vf_lr=1e-3, train_pi_iters=80, train_v_iters=80, lam=0.97, max_ep_len=1000,
-        target_kl=0.01, logger_kwargs=dict(), save_freq=10):
+
+
+class PPO:
+
+    def __init__(self, env, clip_range, gamma, gae_lambda, 
+                 n_steps, n_epochs, batch_size, lr, 
+                 max_grad_norm, value_loss_coef, entropy_coef
+        ):
+        
+        pass
+
+
+        def learn(self):
+
+            
+        
+
+
+
+
+
+
+
