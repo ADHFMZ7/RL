5e482b0677307ad492a818038f6f9669bb6a4e75
adhfmz7 <ahmadaldasouqi559@gmail.com>
2025-08-20 13:08:21 -0700

    edit env

--- src/env.py
+++ src/env.py
@@ -9,9 +9,9 @@ from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.monitor import Monitor
 
 
-def create_pruned_cartpole():
+def create_pruned_cartpole(env_id='custom_cartpole' ,render_mode=None):
     
-    base_env = gym.make("custom_cartpole" )
+    base_env = gym.make(env_id, render_mode=render_mode)
 
     low = np.array([base_env.observation_space.low[0], base_env.observation_space.low[1]])
     high = np.array([base_env.observation_space.high[0], base_env.observation_space.high[1]])
@@ -20,8 +20,8 @@ def create_pruned_cartpole():
     # Create a wrapper environment that removes the pole data from the observation space
     return Monitor(TransformObservation(base_env, lambda obs: obs[:2], pruned_space))
 
-def make_stacked_cartpole(n_envs=1, num_frames=4):
-    env_fn = lambda: create_pruned_cartpole()
+def make_stacked_cartpole(env_id='custom_cartpole', n_envs=1, num_frames=4, render_mode=None):
+    env_fn = lambda: create_pruned_cartpole(env_id=env_id, render_mode=render_mode)
     vec_env = make_vec_env(env_fn, n_envs=n_envs)
     stacked_env = VecFrameStack(vec_env, n_stack=num_frames)
     return stacked_env
